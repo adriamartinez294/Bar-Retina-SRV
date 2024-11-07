e9fc87323bf0ad82acdbed1e542f49da7d7104c2
adriamartinez294 <adriamartinez870@gmail.com>
2024-11-07 17:17:55 +0100

    mas tests

--- Server/run.sh
+++ Server/run.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
-
-# Exemple de funcionament: ./run.sh com.project.Main
-# on 'Main' és la classe amb 'main' que volem executar
-
-# run.sh
-
-# Set MAVEN_OPTS environment variable
-export MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED -Dfile.encoding=UTF8"
-
-# Resta de l'script
-
-# Check for the first argument and set it as the main class
 mainClass=$1
+action=$2 
 
 echo "Setting MAVEN_OPTS to: $MAVEN_OPTS"
 echo "Main Class: $mainClass"
 
-# Execute mvn command with the profile and main class as arguments
-execArg="-PrunMain -Dexec.mainClass=$mainClass"
-echo "Exec args: $mainClass"
+if [[ "$action" == "build" ]]; then
+    echo "Generating JAR file with all dependencies..."
+    mvn clean package -Dmaven.test.skip=true
+    echo "JAR generated in target directory."
+    if [ -f target/server-package.jar ]; then
+        echo "Successfully generated JAR: target/server-package.jar"
+    else
+        echo "Failed to generate JAR."
+        exit 1
+    fi
+else
+    # Execute mvn command with the profile and main class as arguments
+    execArg="-PrunMain -Dexec.mainClass=$mainClass -Djavafx.platform=$javafx_platform"
+    echo "Exec args: $execArg"
 
-# Execute mvn command
-mvn -q clean test-compile exec:java $execArg
+    # Execute mvn command
+    mvn clean test-compile exec:java $execArg -X
+fi
