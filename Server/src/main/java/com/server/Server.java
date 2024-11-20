5dfe1d05913c150e70f780e91e6dfcdb9fa8fe5b
adriamartinez294 <adriamartinez870@gmail.com>
2024-11-20 15:40:38 +0100

    Si señor

--- Server/src/main/java/com/server/Server.java
+++ Server/src/main/java/com/server/Server.java
@@ -150,6 +150,16 @@ public class Server extends WebSocketServer {
 
                         conn.send(msg3.toString());
                         break;
+
+                    case "ready":
+                        JSONObject msg4 = new JSONObject();
+
+                        String tableMessage = obj.getString("message");
+                        msg4.put("type", "ready");
+                        msg4.put("message", tableMessage);
+
+                        broadcastMessage(msg4.toString(), null);
+
                 }
             }
         }
